afaba02a908aef7ed088a344b95048995690b0ab
Mikhail <mihdan@yandex.ru>
2014-01-30 01:45:27 +0400

    Изменил версию

--- object-cache.php
+++ object-cache.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Memcached
 Description: Memcached backend for the WP Object Cache.
-Version: 2.0.3
+Version: 2.0.4
 Plugin URI: http://wordpress.org/extend/plugins/memcached/
 Author: Ryan Boren, Denis de Bernardy, Matt Martz, Mikhail Kobzarev
 
